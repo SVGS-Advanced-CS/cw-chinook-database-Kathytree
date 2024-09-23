f781cd47252a4ae9e724ad462f1b414ae67bf069
KathyD <dedrickkj@svgs.k12.va.us>
2024-09-23 10:03:15 -0400

    '

--- src/main/java/com/svgs/Main.java
+++ src/main/java/com/svgs/Main.java
@@ -1,10 +1,20 @@
 package com.svgs;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
         String url = "jdbc:sqlite:./src/main/resources/chinook.db";
+        try { 
+            Connection conn = DriverManager.getConnection(url);
 
+            conn.close();
+            
+        } catch (SQLException e) {
+            System.out.println("error :(");
+        }
 
     }
 }
