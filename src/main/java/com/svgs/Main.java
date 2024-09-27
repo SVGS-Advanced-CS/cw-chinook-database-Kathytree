f3c2b1e670c4ee321233202f7a2c2b69a30c3973
KathyD <dedrickkj@svgs.k12.va.us>
2024-09-27 10:05:11 -0400

    cat

--- src/main/java/com/svgs/Main.java
+++ src/main/java/com/svgs/Main.java
@@ -1,5 +1,6 @@
 package com.svgs;
 
+import java.util.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -7,13 +8,50 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Main {
+
+    private static Connection conn;
+    private static Statement state;
     public static void main(String[] args) {
-        String url = "jdbc:sqlite:./src/main/resources/chinook.db";
+        createDB();
+        Scanner scan = new Scanner(System.in);
+        System.out.println("1. Create a user");
+        String choice = scan.nextLine();
+        if(choice.equals("1"))
+        {
+            System.out.println("Username:");
+            String username = scan.nextLine();
+            System.out.println("Password:");
+            String password = scan.nextLine();
+            System.out.println("ROle:");
+            String role = scan.nextLine();
+            createUser(username,password,role);
+        }
+    }
+    public static void menu(){
+
+    }
+    public static void createUser(String u, String p, String r)
+    {
+        
+    }
+    public static void createDB(){
+        String url = "jdbc:sqlite:./src/main/resources/users.db";
+        try{
+            conn = DriverManager.getConnection(url);
+            state = conn.createStatement();
+            String query = "CREATE TABLE IF NOT EXIST users(userId TEXT, password TEXT, role TEXT)";
+            state.executeUpdate(query);
+        } catch (Exception e) {
+            System.out.println("you effed up.");
+        }
+
+    }
+       /*  String url = "jdbc:sqlite:./src/main/resources/users.db";
         try { 
             Connection conn = DriverManager.getConnection(url);
             Statement state = conn.createStatement();
 
-            String query = "INSERT INTO employees(FirstName,LastName) VALUES('Sherri','Berry')";
+            String query = "UPDATE employees SET FirstName = 'Garfield', LastName = 'Andrew' WHERE FirstName='Andrew' AND LastName='Adams'";
             state.executeUpdate(query);
 
             query = "DELETE FROM employees WHERE FirstName='Sherri'";
@@ -34,5 +72,5 @@ public class Main {
             System.out.println("error :(");
         }
 
-    }
+    }*/
 }
